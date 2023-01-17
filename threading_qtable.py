6533421e385e61ae0f0ffba146fd68c694a82e88
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-01-17 08:12:47 +0700

    Update threading_qtable.py

--- threading_qtable.py
+++ threading_qtable.py
@@ -8,30 +8,34 @@ from gui import Ui_MainWindow
 
 
 class MainWindow(QMainWindow):
-    signal = pyqtSignal(int)
+    signal = pyqtSignal(object)
 
     def __init__(self):
         super().__init__()
         self.a = 0
         self.uic = Ui_MainWindow()
         self.uic.setupUi(self)
-        self.uic.pushButton.clicked.connect(self.jj)
+        self.uic.pushButton.clicked.connect(self.start_thread)
+        # self.signal.connect(self.update_text)
 
-    def jj(self):
-        t = threading.Thread(target=self.hh)
+    def start_thread(self):
+        # self.uic.tableWidget.setItem(1, 1, QTableWidgetItem(str(self.a)))
+        # self.uic.label.setText(str(self.a))
+
+        t = threading.Thread(target=self.job)
         t.start()
 
-    def hh(self):
+    def job(self):
         while True:
             print(self.a)
             self.signal.emit(self.a)
-            self.signal.connect(self.update1)
+            self.signal.connect(self.update_text)
             time.sleep(1)
             self.a += 1
             if self.a == 5:
                 break
 
-    def update1(self):
+    def update_text(self):
         self.uic.tableWidget.setItem(1, 1, QTableWidgetItem(str(self.a)))
         self.uic.label.setText(str(self.a))
 
